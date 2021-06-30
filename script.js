3eee985b6bbc23eeb62445e69c26065720c89171
Marcos Trejo <marcostrejo23@gmail.com>
2021-06-29 21:07:27 -0700

    pushing timer as well as start button changes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let examBox = document.getElementById("exam");
+let examBox = document.getElementById("#exam");
 let startButton = document.getElementById("start");
 let submitButton = document.getElementById("submit");
 let resultspage = document.getElementById("results");
+var timeEl = document.getElementById("#time");
 function startQuiz(){};
 function showResults(){};
 startButton.addEventListener('click',startQuiz);
 submitButton.addEventListener('click', Results);
-let questionNumber= ("question1","question2","question3")
+// let questionNumber= ("question1","question2","question3")
 
 let theQuestions = [{
-        Question1: "What is preventing default?",
+        Question: "What is preventing default?",
         answers: {
             a: "Banana",
             b: "If the event does not get explicity handled, its defaul action should not be taken as normal.",
@@ -18,7 +19,7 @@ let theQuestions = [{
         correctAnswer: "b"
     } ,
     {
-        question2: "What is an array?",
+        question: "What is an array?",
         answers: {
             a: "Stylesheet",
             b: "Banana",
@@ -27,7 +28,7 @@ let theQuestions = [{
         correctAnswer: "c",
     },
     {
-        question3: "what is append used for in js?",
+        question: "what is append used for in js?",
         answers:{
             a: "banana",
             b: "insert a set of objects or DOMstring objects.",
@@ -36,19 +37,45 @@ let theQuestions = [{
         correctAnswer: "b",
     }
 ];
-function createQuiz(){
-    let output = [];
-    theQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-        let answers = [];
-        for(letter in theQuestions.answers){
-            answers.push(
-                <label>
-                <input type= "radio" name ="question${questionNumber}" value= "${letter}"></input>
-                ${letter};
-                ${currentQuestion.answers[letter]}
-                </label>
-            )
-        }
-        output.push(answers );
-        examBox.innerHTML = output.join('')}
+
+// setInterval()
+// startButton.addEventListener("start", startButton){
+//     for (let i =0;i<theQuestions.length; i++)
+// }
+
+function startButton() {
+    onclick.theQuestions.getElementById(theQuestions);
+    var timeInterval = setInterval(function(){
+        secondsLeft--;
+        timeInterval.textContent = secondsLeft + "seconds left in exam";
+        
+        if (secondsLeft=== 0){
+            clearInterval(timeInterval);
+            sendMessage();
+        } 
+    }, 1000);
+}
+function sendMessage(){
+    timeEL.textContent = '';
+    return resultspage();
+}
+startButton();
+
+
+    
+// function createQuiz(){
+//     let output = [];
+//     theQuestions.forEach(
+//         (currentQuestion, questionNumber) => {
+//         let answers = [];
+//         for(letter in theQuestions.answers){
+//             answers.push(
+//                 <label>
+//                 <input type= "radio" name ="question${questionNumber}" value= "${letter}"></input>
+//                 ${letter};
+//                 ${currentQuestion.answers[letter]}
+//                 </label>
+//             )
+//         }
+//         output.push(answers );
+//         examBox.innerHTML = output.join('')}
