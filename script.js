fa06ddc59cbc31eb4446e4158b26719a1b89e7af
Marcos Trejo <marcostrejo23@gmail.com>
2021-09-05 16:27:26 -0700

    changed global variables

--- script.js
+++ script.js
@@ -1,75 +1,110 @@
-let examBox = document.getElementById("#exam");
-let startButton = document.getElementById("start-button");
-let submitButton = document.getElementById("submit");
-let resultsPage = document.getElementById("results");
-var timeEl = document.getElementById("#time");
-let answerNum = 0
-function startQuiz(){};
-function showResults(){};
+// let examBox = document.getElementById("#exam");
+// let startButton = document.getElementById("start-button");
+// let submitButton = document.getElementById("submit");
+// let resultsPage = document.getElementById("results");
+// var timeEl = document.getElementById("#time");
+// let answerNum = 0
+// function startQuiz(){};
+// function showResults(){};
 
-startButton.addEventListener('click', start);
+// startButton.addEventListener('click', start);
 
-var theQuestions = [
-    {
-        question: "What is preventing default?",
-        answers:[
-            "Banana",
-            "If the event does not get explicity handled, its default action should not be taken as normal.",
-           "List like object, has methods to perform operations"
-        ],
+// var theQuestions = [
+//     {
+//         question: "What is preventing default?",
+//         answers:[
+//             "Banana",
+//             "If the event does not get explicity handled, its default action should not be taken as normal.",
+//            "List like object, has methods to perform operations"
+//         ],
         
-        correctAnswer: 1
-    } ,
-    {
-        question: "What is an array?",
-        answers: [ "Stylesheet", "Banana", "List like object, has methods to perform operations."
-        ],
-        correctAnswer: 2,
-    },
-    {
-        question: "what is append used for in js?",
-        answers:{
-            a: "banana",
-            b: "insert a set of objects or DOMstring objects.",
-            c: "returns an element whose id property matches the specific string.",
-                },
-        correctAnswer: 1,
-    }
-];
+//         correctAnswer: 1
+//     } ,
+//     {
+//         question: "What is an array?",
+//         answers: [ "Stylesheet", "Banana", "List like object, has methods to perform operations."
+//         ],
+//         correctAnswer: 2,
+//     },
+//     {
+//         question: "what is append used for in js?",
+//         answers:{
+//             a: "banana",
+//             b: "insert a set of objects or DOMstring objects.",
+//             c: "returns an element whose id property matches the specific string.",
+//                 },
+//         correctAnswer: 1,
+//     }
+// ];
 
 
-//when the user clicks start, 
-TODO://the start button will hide, 
-function start() {
-  onclick= document.getElementById("code-header").textContent = theQuestions[answerNum].question;
-  //question options will appear 
-  //they will be presented with the first question. question i and its three choices
-  // they will be presented with three choices 
-  //each choice will have a button 
-  document.getElementById("answer1").textContent =theQuestions[answerNum].answers[0];
-  document.getElementById("answer2").textContent =theQuestions[answerNum].answers[1];
-  document.getElementById("answer3").textContent =theQuestions[answerNum].answers[2];
+// //when the user clicks start, 
+// TODO://the start button will hide, 
+// function start() {
+//   onclick= document.getElementById("code-header").textContent = theQuestions[answerNum].question;
+//   //question options will appear 
+//   //they will be presented with the first question. question i and its three choices
+//   // they will be presented with three choices 
+//   //each choice will have a button 
+//   document.getElementById("answer1").textContent =theQuestions[answerNum].answers[0];
+//   document.getElementById("answer2").textContent =theQuestions[answerNum].answers[1];
+//   document.getElementById("answer3").textContent =theQuestions[answerNum].answers[2];
 
 
 
-  console.log(theQuestions);
-  console.log(theQuestions[0]);
-  console.log(theQuestions[0].answers[1]);
+//   console.log(theQuestions);
+//   console.log(theQuestions[0]);
+//   console.log(theQuestions[0].answers[1]);
 
-}
-document.getElementById("answerDiv").addEventListener("click", function(){
-    //user will select their choice 
-    //if they choose wrong, timer will decrease. 
-    var answer = theQuestions[answerNum].correctAnswer
-    console.log(answer)
+// }
+// document.getElementById("answerDiv").addEventListener("click", function(){
+//     //user will select their choice 
+//     //if they choose wrong, timer will decrease. 
+//     var answer = theQuestions[answerNum].correctAnswer
+//     console.log(answer)
     
-//if our answer is = to the button we clicked. 
+// //if our answer is = to the button we clicked. 
 
 
-    //if they choose correct, timer is unchanged. 
-    //once first question is answered, 
-console.log(event.target)
-console.log(event.target.getAttribute("data-index"));
+//     //if they choose correct, timer is unchanged. 
+//     //once first question is answered, 
+// console.log(event.target)
+// console.log(event.target.getAttribute("data-index"));
 
-})
-cc
+// })
+// cc
+
+let question1={
+    questions: "What is preventing default?",
+    numberedAnswers= 3,
+    correctA="1",
+
+    answer0: "Banana",
+    answer1: "If the event does not get explicity handled, its default action should not be taken as normal.",
+    answer2: "List like object, has methods to perform operations"
+}
+let question2={
+    questions: "What is an array?",
+    numberedAnswers= 3,
+    correctA="1",
+
+    answer0: "Stylesheet",
+    answer1: "List like object",
+    answer2: "has methods to perform operations"
+}
+let question3={
+    questions: "What is append used for?",
+    numberedAnswers= 3,
+    correctA="1",
+
+    answer0: "banana",
+    answer1: "insert a set of objects or DOMstring objects",
+    answer2: "returns an element whose id property matches the specific string"
+}
+
+
+let headerEL=document.querySelector("#informationHeader");
+let contentArea =document.querySelector("#contentArea");
+let infoBox= document.querySelector("#infoBox");
+let mainEL= document.querySelector("#main");
+let timerEL=document.querySelector("#timer");
