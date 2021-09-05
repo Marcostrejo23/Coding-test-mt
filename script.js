ca644a0e7ecb6d7f2ea7a0ed389645e2082e6633
Marcos Trejo <marcostrejo23@gmail.com>
2021-09-05 16:10:12 -0700

    cleaning up html

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 let examBox = document.getElementById("#exam");
 let startButton = document.getElementById("start-button");
 let submitButton = document.getElementById("submit");
-let resultspage = document.getElementById("results");
+let resultsPage = document.getElementById("results");
 var timeEl = document.getElementById("#time");
 let answerNum = 0
 function startQuiz(){};
 function showResults(){};
-// startButton.addEventListener('click',startQuiz);
-// submitButton.addEventListener('click', Results);
-// let questionNumber= ("question1","question2","question3")
 
 startButton.addEventListener('click', start);
 
-let theQuestions = [
+var theQuestions = [
     {
         question: "What is preventing default?",
         answers:[
             "Banana",
-            "If the event does not get explicity handled, its defaul action should not be taken as normal.",
+            "If the event does not get explicity handled, its default action should not be taken as normal.",
            "List like object, has methods to perform operations"
         ],
         
@@ -41,11 +38,6 @@ let theQuestions = [
 ];
 
 
-// setInterval()
-// startButton.addEventListener("start", startButton){
-//     for (let i =0;i<theQuestions.length; i++)
-// }
-
 //when the user clicks start, 
 TODO://the start button will hide, 
 function start() {
@@ -64,22 +56,6 @@ function start() {
   console.log(theQuestions[0]);
   console.log(theQuestions[0].answers[1]);
 
-
-//   return.theQuestions;
-   
-//     var timeInterval = setInterval(function(){
-//         secondsLeft--;
-//         timeInterval.textContent = secondsLeft + "seconds left in exam";
-        
-//         if (secondsLeft=== 0){
-//             clearInterval(timeInterval);
-//             sendMessage();
-//         } 
-//     }, 999);
-// }
-// function sendMessage(){
-//     timeEL.textContent = '';
-//     return resultspage();
 }
 document.getElementById("answerDiv").addEventListener("click", function(){
     //user will select their choice 
@@ -96,27 +72,4 @@ console.log(event.target)
 console.log(event.target.getAttribute("data-index"));
 
 })
-
-// function createQuiz(){
-//     let output = [];
-//     theQuestions.forEach(
-//         (currentQuestion, questionNumber) => {
-//         let answers = [];
-//         for(letter in theQuestions.answers){
-//             answers.push(
-//                 <label>
-//                 <input type= "radio" name ="question${questionNumber}" value= "${letter}"></input>
-//                 ${letter};
-//                 ${currentQuestion.answers[letter]}
-//                 </label>
-//             )
-//         }
-//         output.push(answers );
-//         examBox.innerHTML = output.join('')}
-
-
-
-//presented with second question. 
-//user will select their choice 
-//if they choose wrong, timer will decrease. 
-//if they choose correct, timer is unchanged. 
+cc
