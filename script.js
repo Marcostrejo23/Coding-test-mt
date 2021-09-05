8cbd548235ce02b6c08f42e5766b07c803d1c0e6
Marcos Trejo <marcostrejo23@gmail.com>
2021-09-05 16:40:40 -0700

    cleaning up the file

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
+
+
+let question1={
+    questions: "What is preventing default?",
+    numberedAnswers= 3,
+    correctA="1",
+
+    answer0: "Banana",
+    answer1: "If the event does not get explicity handled, its default action should not be taken as normal.",
+    answer2: "List like object, has methods to perform operations"
+}
+let question2={
+    questions: "What is an array?",
+    numberedAnswers= 3,
+    correctA="1",
+
+    answer0: "Stylesheet",
+    answer1: "List like object",
+    answer2: "has methods to perform operations"
+}
+let question3={
+    questions: "What is append used for?",
+    numberedAnswers= 3,
+    correctA="1",
+
+    answer0: "banana",
+    answer1: "insert a set of objects or DOMstring objects",
+    answer2: "returns an element whose id property matches the specific string"
+}
+
+
+let headerEL=document.querySelector("#informationHeader");
+let contentArea =document.querySelector("#contentArea");
+let infoBox= document.querySelector("#infoBox");
+let mainEL= document.querySelector("#main");
+let timerEL=document.querySelector("#timer");
+
+let quizLength = 120;
+let quizIndex = 0;
+let quizScored= 0;
+let completedQuiz= false;
+let questions = [question1,question2,question3];
+let highScores= [];
+let quizTimer;
+
+function init(){
+    let renderScores = JSON.parse(localStorage.getItem("scorecard"));
+    if(renderScores !== null){
+        highScores=renderScores;
+    }
+    renderMainPage();
+}
+
+function renderMainPage(){
+    let pTag = document.createElement("p");
+    let quizButton= document.createElement("button");
+    mainEL.style.setProperty("--qAlignment","center");
+    mainEL.style.setProperty("--content-width","70vw");
+    headerEL.textContent= "Take the coding quiz";
+    contentArea.textContent="";
+    infoBox.textContent="";
+    pTag.textContent="Answer all the questions in the allotted time";
+    quizButton.textContent="Let's go!";
+    quizButton.setAttribute("id","quiz-Button");
+    quizButton.addEventListener("click", "startQuiz");
+    contentArea.append(pTag);
+    contentArea.append(quizButton);
+}
+
 // let examBox = document.getElementById("#exam");
 // let startButton = document.getElementById("start-button");
 // let submitButton = document.getElementById("submit");
@@ -73,70 +142,3 @@
 
 // })
 // cc
-
-let question1={
-    questions: "What is preventing default?",
-    numberedAnswers= 3,
-    correctA="1",
-
-    answer0: "Banana",
-    answer1: "If the event does not get explicity handled, its default action should not be taken as normal.",
-    answer2: "List like object, has methods to perform operations"
-}
-let question2={
-    questions: "What is an array?",
-    numberedAnswers= 3,
-    correctA="1",
-
-    answer0: "Stylesheet",
-    answer1: "List like object",
-    answer2: "has methods to perform operations"
-}
-let question3={
-    questions: "What is append used for?",
-    numberedAnswers= 3,
-    correctA="1",
-
-    answer0: "banana",
-    answer1: "insert a set of objects or DOMstring objects",
-    answer2: "returns an element whose id property matches the specific string"
-}
-
-
-let headerEL=document.querySelector("#informationHeader");
-let contentArea =document.querySelector("#contentArea");
-let infoBox= document.querySelector("#infoBox");
-let mainEL= document.querySelector("#main");
-let timerEL=document.querySelector("#timer");
-
-let quizLength = 120;
-let quizIndex = 0;
-let quizScored= 0;
-let completedQuiz= false;
-let questions = [question1,question2,question3];
-let highScores= [];
-let quizTimer;
-
-function init(){
-    let renderScores = JSON.parse(localStorage.getItem("scorecard"));
-    if(renderScores !== null){
-        highScores=renderScores;
-    }
-    renderMainPage();
-}
-
-function renderMainPage(){
-    let pTag = document.createElement("p");
-    let quizButton= document.createElement("button");
-    mainEL.style.setProperty("--qAlignment","center");
-    mainEL.style.setProperty("--content-width","70vw");
-    headerEL.textContent= "Take the coding quiz";
-    contentArea.textContent="";
-    infoBox.textContent="";
-    pTag.textContent="Answer all the questions in the allotted time";
-    quizButton.textContent="Let's go!";
-    quizButton.setAttribute("id","quiz-Button");
-    quizButton.addEventListener("click", "startQuiz");
-    contentArea.append(pTag);
-    contentArea.append(quizButton);
-}
